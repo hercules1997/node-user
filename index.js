8261987390c442d9137fd74ecf13febbd383cb5a
hercules1997 <hercules.chaves.andrade@gmail.com>
2022-12-27 20:58:49 -0300

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const uuid = require('uuid')
 const cors = require("cors")
 
-const port = 3019
+const port = express
 const app = express()
 app.use(express.json())
 app.use(cors())
